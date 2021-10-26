ccfebac14564a2b38b04792eac50c531a9add166
jooo0922 <2014102458@khu.ac.kr>
2021-10-26 14:15:47 +0900

    feat: Use mix() built-in func

--- color.glsl
+++ color.glsl
@@ -5,26 +5,56 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
+vec3 colorA = vec3(0.149, 0.141, 0.912);
+vec3 colorB = vec3(1.000, 0.833, 0.224);
+
 void main() {
-  vec3 color = vec3(0., 0., 0.);
-  color.z = 0.8;
+  vec3 color = vec3(0.0);
 
-  vec3 yellow, magenta, green; // 만들고자 하는 색상 변수들을 선언함.
+  // sin은 u_time의 사인값을 리턴해주므로, 시간의 경과에 따라 -1 ~ 1 사이의 값을 주기적으로 리턴해줄거임.
+  // 반면 abs는 모든 값을 절댓값으로 변환해주니까, 사인값을 0 ~ 1 사이의 값이 오가도록 변환해줄거임. 
+  float pct = abs(sin(u_time));
 
-  // Making Yellow
-  yellow.rg = vec2(1.);
-  yellow[2] = 0.; // 결과적으로 vec3 yellow 에는 (1., 1., 0.) 이 들어가게 됨. 만약 [2] 번째 값을 따로 할당하지 않으면 해당 성분에는 알아서 0.0 이 자동으로 할당됨.
+  // Mix uses pct (a value from 0-1) to
+  // mix the two colors
+  // color = colorB * pct + colorA * (1. - pct); // smoothstep 예제에서 배웠듯이 아래의 mix() 내장함수를 그대로 구현한 코드라고 보면 됨.
+  // 즉, 위에서 살펴봤듯이, pct는 0 ~ 1 사이의 값을 오가는데,
+  // 이 때, pct가 0이면 color에는 colorA 값이 들어가고, pct가 1이면 color에는 colorB 값이 들어가게 됨.
+  // pct가 0 ~ 1 사이라면, 위의 공식에 따라 두 색상을 pct 비율에 맞게 혼합해서 나오겠지
+  color = mix(colorA, colorB, pct); // 이제 내장함수 mix() 를 활용해서 좀 더 편하게 코딩하면 됨.
 
-  // Making Magenta
-  // yellow.rgb = (1., 1., 0.)이기 때문에, 두 번째와 세 번째 성분 순서를 바꿔서 할당하기 위해 yellow.rbg 로 넣어준 것.
-  // 따라서 vec3 magenta 에는 (1., 0., 1.) 이 들어가게 됨.
-  // 이처럼 어떤 값의 r, g, b 성분값들을 동시에 할당할 때, 반드시 순서를 지켜야 한다는 고정관념을 버릴 것.
-  // 위에 방법 말고도, yellow.rrr 이런 식으로 하나의 성분값만 반복해서 넣어줄수도 있음.
-  // 이런 식으로 r, g, b, a / x, y, z, w 의 값을 마음대로 순서를 바꿔가면서 벡터의 성분에 접근하는 것을 'swizzle'이라고 함. 
-  magenta = yellow.rbg;
-
-  gl_FragColor = vec4(magenta, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
+
+/*
+  #ifdef GL_ES
+  precision mediump float;
+  #endif
+
+  uniform vec2 u_resolution;
+  uniform float u_time;
+
+  void main() {
+    vec3 color = vec3(0., 0., 0.);
+    color.z = 0.8;
+
+    vec3 yellow, magenta, green; // 만들고자 하는 색상 변수들을 선언함.
+
+    // Making Yellow
+    yellow.rg = vec2(1.);
+    yellow[2] = 0.; // 결과적으로 vec3 yellow 에는 (1., 1., 0.) 이 들어가게 됨. 만약 [2] 번째 값을 따로 할당하지 않으면 해당 성분에는 알아서 0.0 이 자동으로 할당됨.
+
+    // Making Magenta
+    // yellow.rgb = (1., 1., 0.)이기 때문에, 두 번째와 세 번째 성분 순서를 바꿔서 할당하기 위해 yellow.rbg 로 넣어준 것.
+    // 따라서 vec3 magenta 에는 (1., 0., 1.) 이 들어가게 됨.
+    // 이처럼 어떤 값의 r, g, b 성분값들을 동시에 할당할 때, 반드시 순서를 지켜야 한다는 고정관념을 버릴 것.
+    // 위에 방법 말고도, yellow.rrr 이런 식으로 하나의 성분값만 반복해서 넣어줄수도 있음.
+    // 이런 식으로 r, g, b, a / x, y, z, w 의 값을 마음대로 순서를 바꿔가면서 벡터의 성분에 접근하는 것을 'swizzle'이라고 함. 
+    magenta = yellow.rbg;
+
+    gl_FragColor = vec4(magenta, 1.0);
+  }
+*/
 
 /*
   이미 만들어놓은 vec3 색상값의 각 성분을 접근 및 수정하는 법
